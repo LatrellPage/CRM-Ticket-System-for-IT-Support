3a40268af08898ec425511db37bafb9f74642f6b
Latrell Page <pagelatrell14@gmail.com>
2023-09-14 19:13:18 -0500

    Removed priority model

--- server copy/models/index.js
+++ server copy/models/index.js
@@ -2,9 +2,8 @@ const User = require('./User');
 const Ticket = require('./Ticket');
 const Category = require('./Category');
 const Admin = require('./Admin');
-const Priority = require('./Priority');
 
-module.exports = { User, Ticket, Category, Admin, Priority};
+module.exports = { User, Ticket, Category, Admin};
 
 // Imports all models into one object, making it easer to use in other places of the application
 // You can import model and access each model using module.{model name}
