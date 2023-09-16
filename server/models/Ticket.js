caf3664e1cdad435a5cbb155e95211b171d49ae1
wygrajr <wycliffe.graftonjr@gmail.com>
2023-09-15 22:15:58 -0400

    index model was updated to remove Admin model reference. Priority was commented out in Ticket model (do not see Priority model) and assigned property modified to computed field. Updated typeDef schema with types, enums and mutations

--- server/models/Ticket.js
+++ server/models/Ticket.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 const Category = require('./Category');
-const Admin = require('./Admin');
-const Priority = require('./Priority');
+// const Priority = require('./Priority');
 const User = require('./User');
 
 const ticketSchema = new mongoose.Schema({
   title: String,
   content: String,
   createdAt: Date,
-  priority: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: Priority, // References the priority of the ticket
-  },
+  // priority: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: Priority, // References the priority of the ticket
+  // },
   category: {
     type: mongoose.Schema.Types.ObjectId,
     ref: Category, // References the category of the ticket
@@ -22,17 +21,17 @@ const ticketSchema = new mongoose.Schema({
   },
   admin: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: Admin, // References the admin that is assigned to the ticket
+    ref: User, // References the admin that is assigned to the ticket
   },
   status: {
     type: String,
     enum: ['active', 'complete'],
     default: 'active', // Tickets are initially set as active
   },
-  assigned: {
-    type: Boolean,
-    default: true, // Tickets are initially unassigned
-  },
+});
+
+ticketSchema.virtual('assigned').get(function () {
+  return this.user != null;
 });
 
 const Ticket = mongoose.model('Ticket', ticketSchema);
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,20 +6,75 @@ const typeDefs = gql`
         name:String!
         email:String!
         role:String!
+        assignedTickets:String!
+        skills:String!
     }
 
     type Query {
         me: User
     }
 
+    type Ticket {
+        ticketId:Int!
+        title:String!
+        content:String!
+        createdAt:Date!
+        category:String!
+        user:User!
+        admin:User!
+        status:String!
+        assigned:Boolean
+    }
+
+    type Category{
+        name:User!
+    }
+
+    enum Roles{
+        admin,
+        user
+    }
+
+    enum Status{
+        active,
+        complete,
+    }
+
     type Auth {
         token: ID!
         user: User
     }
-    
+
     type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
+        signIn(
+            email: String!,
+            password: String!
+            ): Auth
+        signUp(
+            name: String!,
+            email: String!,
+            password: String!
+            ): Auth
+        createTicket(
+            title:String!,
+            content:String!,
+            createdAt:Date!,
+            category:String!,
+            admin:User!,
+            status:String!
+            ):Ticket
+        getTicket(
+            ticketId:Int!
+            ):Ticket
+        getUserTickets(): [Ticket]!
+        getAllTickets(): [Ticket]!
+        assignTicket(
+            user:User!,
+            ticketId:Int!
+        ):Ticket
+        deleteTicket(
+            ticketId:Int!
+        )
     }
 
 
